a9111c48fab3676b5953e2d1d1f177d68830b818
Alfredo Maldonado <alfredorafael1995@hotmail.com>
2026-02-18 10:02:21 +0100

    Initial Commit

---
+++ Curso-ios-GestionAlumnos-SwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Curso-ios-GestionAlumnos-SwiftData
+//
+//  Created by Equipo 8 on 18/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Curso-ios-GestionAlumnos-SwiftData/Curso_ios_GestionAlumnos_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Curso_ios_GestionAlumnos_SwiftDataApp.swift
+//  Curso-ios-GestionAlumnos-SwiftData
+//
+//  Created by Equipo 8 on 18/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Curso_ios_GestionAlumnos_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
